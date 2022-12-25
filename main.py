f3a0aef6c7a93be9ef2782ec40154711c104ebd4
ravindaa <ravindaa@oregonstate.edu>
2022-12-25 10:10:11 +0200

    Added x label

--- main.py
+++ main.py
@@ -17,10 +17,13 @@ figure = plt.Figure(figsize=(5, 4), dpi=100)
 # Input is number of rows, number of cols, index position
 # Assumes a grid layout
 figure_plot = figure.add_subplot(1, 1, 1)
+figure_plot.set_xlabel('Years')
+figure_plot.set_ylabel('Unemployment Rate')
 # Place figure on main window
 line = FigureCanvasTkAgg(figure, main_window)
 # get_tk_widget
 line.get_tk_widget().pack(side=tk.LEFT, fill=tk.BOTH)
+# Meant for actual dates - groups unemployment rates by year and adds them up
 dataframe = dataframe[['year', 'unemployment_rate']].groupby('year').sum()
 dataframe.plot(kind='line', legend=True, ax=figure_plot, color='r', marker='o', fontsize=10)
 figure_plot.set_title('Year Vs. Unemployment Rate')
